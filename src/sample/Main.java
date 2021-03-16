85e5ed790c94e73665d25d802adde052cbbf1b4f
timofeya123 <timofeya123@inbox.ru>
2021-03-16 18:50:49 +0600

    ScaleTransition

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import com.sun.javafx.geom.RectBounds;
 import com.sun.javafx.geom.Shape;
 import com.sun.javafx.geom.transform.BaseTransform;
 import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -34,19 +35,42 @@ public class Main extends Application {
         root.setVgap(10);
         root.setHgap(70);
 
-        // создание квадрата
+        // создание квадратов
         Rectangle rectangle = new Rectangle(100,100, Color.RED);
         Rectangle rectangle2 = new Rectangle(100, 100, Color.GREEN);
-        rectangle2.setRotate(50.0);
+        rectangle2.setRotate(50.0); // повернуть боком (ромб)
+        rectangle2.setX(100);
+        rectangle2.setY(100);
         Rectangle rectangle3 = new Rectangle(100, 100, Color.YELLOW);
 
+        //-------------------------------------------- RotateTransition -----------
         // действия на нжатия мышки
         rectangle.setOnMouseClicked(event -> { // анимация поворота, время / на каком объекте
             RotateTransition rt = new RotateTransition(Duration.seconds(2), rectangle);
-            rt.setByAngle(125); // на сколько градусов повернуть
+            rt.setByAngle(180); // на сколько градусов повернуть
             rt.setCycleCount(3); // сколько раз повторить анимацию?
             rt.play(); // отыгать анимацию
         });
+
+        //-------------------------------------------- ScaleTransition -----------
+        // действия при наведении мышки
+        rectangle2.setOnMouseEntered(event -> { // анимация увеличения объекта, время / на каком объекте
+            ScaleTransition st = new ScaleTransition(Duration.seconds(1), rectangle2);
+            st.setToX(2); // до каких размеров изменять х2
+            st.setToY(2);
+            st.play(); // отыгать анимацию
+        });
+
+        // действия при отведении мышки
+        rectangle2.setOnMouseExited(event -> { // анимация увеличения объекта, время / на каком объекте
+            ScaleTransition st = new ScaleTransition(Duration.seconds(1), rectangle2);
+            st.setFromX(2.5); // от каких размеров изменять х2.5
+            st.setFromY(2.5);
+            st.setToX(1); // до каких размеров изменять х1
+            st.setToY(1);
+            st.play(); // отыгать анимацию
+        });
+
 
         // расположение на форме
         GridPane.setConstraints(rectangle, 0, 0);
